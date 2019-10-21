706a929f8f0fcde522f5e3e4226c1196ca3d522f
Blomeyer <ulrich.blomeyer@msb.nrw.de>
2019-10-21 07:16:36 +0200

    first commit

---
+++ GT.dpr
@@ -0,0 +1,14 @@
+program GT;
+
+uses
+  Vcl.Forms,
+  mainGT in 'mainGT.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
